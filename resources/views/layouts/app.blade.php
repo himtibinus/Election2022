65628027934fac02f6befa7f45ad7f6c748bcb1c
Louis G <louisgustavo07@gmail.com>
2022-10-06 17:43:22 +0700

    feat: add footer

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -13,6 +13,7 @@
     <link href="{{ asset('css/app.css') }}" rel="stylesheet" type="text/css" />
     <link href="{{ asset('css/style.css') }}" rel="stylesheet" type="text/css" />
     <link href="{{ asset('css/nav.css') }}" rel="stylesheet" type="text/css" />
+    <link href="{{ asset('css/footer.css') }}" rel="stylesheet" type="text/css" />
 
     @yield('assets')
 </head>
@@ -59,10 +60,7 @@
                     <ul class="dropdown-menu mt-3" style="left:auto;right:0;">
                       <li><a class="dropdown-item" href="{{ route('logout') }}"
                         onclick="event.preventDefault();document.getElementById('logout-form').submit();">
-                        <svg class="me-1" xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-box-arrow-left" viewBox="0 0 16 16">
-                          <path fill-rule="evenodd" d="M6 12.5a.5.5 0 0 0 .5.5h8a.5.5 0 0 0 .5-.5v-9a.5.5 0 0 0-.5-.5h-8a.5.5 0 0 0-.5.5v2a.5.5 0 0 1-1 0v-2A1.5 1.5 0 0 1 6.5 2h8A1.5 1.5 0 0 1 16 3.5v9a1.5 1.5 0 0 1-1.5 1.5h-8A1.5 1.5 0 0 1 5 12.5v-2a.5.5 0 0 1 1 0v2z"/>
-                          <path fill-rule="evenodd" d="M.146 8.354a.5.5 0 0 1 0-.708l3-3a.5.5 0 1 1 .708.708L1.707 7.5H10.5a.5.5 0 0 1 0 1H1.707l2.147 2.146a.5.5 0 0 1-.708.708l-3-3z"/>
-                        </svg>
+                        <i class="bi bi-box-arrow-left me-2"></i>
                         {{ __('Logout') }}
                         </a>
                       </li>
@@ -79,6 +77,28 @@
         <main class="mx-2" style="padding-top: 88px">
             @yield('content')
         </main>
+
+        <footer class="blur">
+          <div class="container py-5">
+            <ul class="mb-4 ps-0">
+              <li><a href="/">Home</a></li>
+              <li><a href="/candidates">Candidates</a></li>
+              <li><a href="/vote">Vote</a></li>
+              <li><a href="/login">Login</a></li>
+            </ul>
+            <div class="d-flex justify-content-between align-items-center">
+              <p class="mb-0">© 2022 <a href="https://himti.or.id">HIMTI BINUS University</a>.</p>
+              <div class="socials">
+                <a href="https://www.facebook.com/himtibinus/"><i class="bi bi-facebook"></i></a>
+                <a href="https://twitter.com/himtibinus"><i class="bi bi-twitter"></i></a>
+                <a href="https://www.youtube.com/user/HimtiBinus"><i class="bi bi-youtube"></i></a>
+                <a href="https://instagram.com/himti_binus"><i class="bi bi-instagram"></i></a>
+                <a href="https://github.com/himtibinus/"><i class="bi bi-github"></i></a>
+                <a href="https://www.tiktok.com/@himtibinus"><i class="bi bi-tiktok pe-0"></i></a>
+              </div>
+            </div>
+          </div>
+        </footer>
     </div>
     
     <script src="{{ URL::asset('js/app.js') }}"></script>
