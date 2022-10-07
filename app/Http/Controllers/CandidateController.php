8d71ad56411de1db7ea2f44b0fd126bd596635c1
NatanaelGeraldoS <geradonatanael84@gmail.com>
2022-10-07 14:53:20 +0700

    CHANGE: Email Voted template

--- app/Http/Controllers/CandidateController.php
+++ app/Http/Controllers/CandidateController.php
@@ -38,25 +38,25 @@ class CandidateController extends Controller
     }
     public function update(Request $request, $id)
     {
-        if(Auth::user()->voted == 0){
+        if (Auth::user()->voted == 0) {
             $time_now = Carbon::now()->format('Y-m-d H:i:s');
             $user = User::find($id);
             $user->voted = 1;
             $user->voted_value = $request->get('candidate');
             $user->vote_time = $time_now;
             $candidate = Candidate::find($request->get('candidate'));
-            $candidate->total_vote +=1;
+            $candidate->total_vote += 1;
             $candidate->save();
 
             $user->save();
 
-            if(!str_starts_with(Auth::user()->nim, 'D')) DB::table('email_queue')->insert([
+            if (!str_starts_with(Auth::user()->nim, 'D')) DB::table('email_queue')->insert([
                 'email' => Auth::user()->email,
                 'sender_name' => 'HIMTI Election 2022',
                 'message_type' => 'MARKDOWN',
                 'priority' => -2,
                 'subject' => 'Your vote on HIMTI Election 2022 has been recorded',
-                'message' => 'Thank you for participating in HIMTI Election 2022. Your vote for **Candidate #' . $request->get('candidate') . ': ' . $candidate->name . '** has been successfully recorded.\n\n+ **Voting timestamp (WIB/ICT/UTC+7):** ' . $time_now . '\n+ **Your NIM, KDDSN, or BINUSIAN ID:** ' . $user->nim . '\n\nPlease keep this email safe as a valid evidence of your vote, in case of any disputes of voting results in the future.\n\n**HIMTI Election 2022** is proudly presented and organized by **Himpunan Mahasiswa Teknik Informatika (HIMTI) BINUS University**, an official BINUS University student association for the **Computer Science department** in Bandung, Malang, and Senayan (BINUS International) campuses, as well as **School of Computer Science** in Alam Sutera and Kemanggisan campuses. To learn more about us, please visit [https://himti.or.id](https://himti.or.id) or follow our Instagram account, [@himti_binus](https://instagram.com/himti_binus/).\n\nOne Family, One Goal!',
+                'message' => 'Thank you for participating in HIMTI Election 2022. Your vote for **Candidate #' . $request->get('candidate') . ': ' . $candidate->name . '** has been successfully recorded.<br><br>+ **Voting timestamp (WIB/ICT/UTC+7):** ' . $time_now . '<br>+ **Your NIM, KDDSN, or BINUSIAN ID:** ' . $user->nim . '<br><br>Please keep this email safe as a valid evidence of your vote, in case of any disputes of voting results in the future.<br><br>**HIMTI Election 2022** is proudly presented and organized by **Himpunan Mahasiswa Teknik Informatika (HIMTI) BINUS University**, an official BINUS University student association for the **Computer Science department** in Bandung, Bekasi, Malang, Semarang, and Senayan (BINUS International) campuses, as well as **School of Computer Science** in Alam Sutera and Kemanggisan campuses. <br>To learn more about us, please visit [https://himti.or.id](https://himti.or.id) or follow our Instagram account, [@himti_binus](https://instagram.com/himti_binus/).<br><br>One Family, One Goal!',
                 'created_at' => $time_now
             ]);
             return back();
